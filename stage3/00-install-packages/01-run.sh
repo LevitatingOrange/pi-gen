855cad1a9e17439f7c06be440f9da66549df3fe6
Lennart Vogelsang <lennart@vogelsang.berlin>
2021-12-29 05:52:53 +0100

    added systemd file

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,5 +1,7 @@
 !/bin/bash -e
 
+install -v -o 1000 -g 1000 -m 644 "files/satcom_simulator_client.service" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/satcom_simulator_client.service"
+
 on_chroot << EOF
   sudo setfacl --modify user:$FIRST_USER_NAME:rw /run/podman/podman.sock
 EOF
