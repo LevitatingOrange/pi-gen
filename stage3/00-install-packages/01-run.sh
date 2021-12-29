4792f1c8ac0ea53640a61d939504d96055146b8e
Lennart Vogelsang <lennart@vogelsang.berlin>
2021-12-29 06:03:26 +0100

    Fixed setfacl

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -3,5 +3,5 @@
 install -v -o 1000 -g 1000 -m 644 "files/satcom_simulator_client.service" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/satcom_simulator_client.service"
 
 on_chroot << EOF
-  sudo setfacl --modify user:$FIRST_USER_NAME:rw /run/podman/podman.sock
+  /usr/bin/setfacl --modify user:$FIRST_USER_NAME:rw /run/podman/podman.sock
 EOF
