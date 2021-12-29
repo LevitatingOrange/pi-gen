b622730a7731dbcd5d277604dbed336e45fef0c6
Lennart Vogelsang <lennart@vogelsang.berlin>
2021-12-29 16:40:17 +0100

    added some things that were missing

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -4,9 +4,20 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/"
 install -v -o 1000 -g 1000 -m 644 "files/satcom_simulator_client.service" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/systemd/user/satcom_simulator_client.service"
+install -v -m 644 "files/allow_user_podman_api.service" "${ROOTFS_DIR}/etc/systemd/system/allow_user_podman_api.service"
+install -v -o 1000 -g 1000 -m 600 "secrets/.podman.auth.json" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.podman.auth.json"
 
-# TODO add spi
+# TODO set hostname based on uuid or smth
+# TODO make this workk
 on_chroot << EOF
-  sudo setfacl --modify user:$FIRST_USER_NAME:rw /run/podman/podman.sock
-  sudo setfacl --modify user:$FIRST_USER_NAME:rw /dev/gpiochip0
+  sudo systemctl enable allow_user_podman_api.service
+  sudo usermod -aG spi ${FIRST_USER_NAME}
+  sudo usermod -aG i2c ${FIRST_USER_NAME}
+  sudo usermod -aG gpio ${FIRST_USER_NAME} 
+  sudo loginctl enable-linger ${FIRST_USER_NAME}
 EOF
+#   sudo setfacl --modify user:$FIRST_USER_NAME:rw /run/podman/podman.sock
+# sudo usermod -aG spi levo
+# sudo usermod -aG i2c levo
+# sudo usermod -aG gpio levo  
+# loginctl enable-linger 1000 
