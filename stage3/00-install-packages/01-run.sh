193e892f3baa66e1b938f348bdf1a420a025d6f5
Lennart Vogelsang <lennart@vogelsang.berlin>
2021-12-29 05:24:03 +0100

    initial commit

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,8 +1,5 @@
-#!/bin/bash -e
+!/bin/bash -e
 
 on_chroot << EOF
-update-alternatives --install /usr/bin/x-www-browser \
-  x-www-browser /usr/bin/chromium-browser 86
-update-alternatives --install /usr/bin/gnome-www-browser \
-  gnome-www-browser /usr/bin/chromium-browser 86
+  sudo setfacl --modify user:$FIRST_USER_NAME:rw /run/podman/podman.sock
 EOF
