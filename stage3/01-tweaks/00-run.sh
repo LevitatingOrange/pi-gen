f753d2b03d0804a80c99266addafd15ced35e497
Lennart Vogelsang <dev@vogelsangs.net>
2021-12-31 02:26:40 +0100

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -4,8 +4,9 @@ on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_i2c 1
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_spi 1
-  	su ${FIRST_USER_NAME} -c "systemctl enable --user allow_user_podman_api.service"
-  	su ${FIRST_USER_NAME} -c "systemctl --user enable satcom_simulator_client.service"
+	su ${FIRST_USER_NAME} -c "loginctl enable-linger 1000"
+  	su ${FIRST_USER_NAME} -c "systemctl enable --user podman.socket"
+  	su ${FIRST_USER_NAME} -c "systemctl enable --user satcom_simulator_client.service"
   	sudo usermod -aG spi ${FIRST_USER_NAME}
   	sudo usermod -aG i2c ${FIRST_USER_NAME}
   	sudo usermod -aG gpio ${FIRST_USER_NAME} 
